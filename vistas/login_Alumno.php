46d332e4eaa45c5db9965755c9d38c7f14f953f9
Aimee <zvok59@gmail.com>
2017-06-01 22:14:50 -0500

    aaa

--- vistas/login_Alumno.php
+++ vistas/login_Alumno.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-include_once '../php/conexion.php';
+require_once '../php/conexion.php';
 
 if($alumno->is_loggedin()!="")
 {
