10f6061bdd6d6d6384dba81842fa8a29f6234e5b
enrique1994 <enrique_face1994@hotmail.com>
2018-01-26 10:03:14 -0600

    _
    _

--- vistas/Evaluar.php
+++ vistas/Evaluar.php
@@ -555,7 +555,7 @@ $min=localtime()
 $stmt7 = $DB_con->prepare("SELECT * FROM evaluacion WHERE hora+20 <=$min[1] and id_alumno=:mat and nrc_curso=:id_curso and calif=0");
       $stmt7->bindParam(":id_curso",$ins['id_curso']);
       $stmt7->bindParam(":id_alumno",$userRow['matricula']);
-      $stmt7->execute()
+      $stmt7->execute();
 //$stmt7->execute(array(":id_curso"=>$ins['id_curso'],":mat"=>$userRow['matricula']));
 $cal=$stmt7->fetch(PDO::FETCH_ASSOC);
 
