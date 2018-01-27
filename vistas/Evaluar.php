e945a27128d17dac6eb1d47032630bd36d530d5a
enrique1994 <enrique_face1994@hotmail.com>
2018-01-27 11:59:59 -0600

    _
    _

--- vistas/Evaluar.php
+++ vistas/Evaluar.php
@@ -566,7 +566,7 @@ print $user_id;
 //$stmt7->execute(array(":id_curso"=>$ins['id_curso'],":mat"=>$userRow['matricula']));
 //$cal=$stmt7->fetch(PDO::FETCH_ASSOC);
 //$cal=$stmt7->fetch(PDO::FETCH_ASSOC)
-echo "<input type='text' size=2 value=".$cal['calif']." name=".$cal['id'].">";
+//echo "<input type='text' size=2 value=".$cal['calif']." name=".$cal['id'].">";
   */
   ?>
 </td>
