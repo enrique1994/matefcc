b031e16c1d65ba5841dc0ffee3bfc10522ae256b
enrique1994 <enrique_face1994@hotmail.com>
2018-01-29 11:22:49 -0600

    _
    _

--- php/anadir_criterio2.php
+++ php/anadir_criterio2.php
@@ -24,7 +24,7 @@ while ($alu=$stmt2->fetch(PDO::FETCH_ASSOC)) {
 
 
       
- $stmt3 = $DB_con->prepare("INSERT INTO evaluacion(nrc_curso,id_criterios,id_alumno,hora,maximo_ejer) VALUES(:nrc_curso,:id_criterios,:id_alumno,:hora,:10)");
+ $stmt3 = $DB_con->prepare("INSERT INTO evaluacion(nrc_curso,id_criterios,id_alumno,hora,maximo_ejer) VALUES(:nrc_curso,:id_criterios,:id_alumno,:hora,10)");
       $stmt3->bindParam(":nrc_curso",$curso);
       $stmt3->bindParam(":id_criterios",$id);
       $stmt3->bindParam(":id_alumno",$est);
