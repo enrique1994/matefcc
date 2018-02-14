0c8459b70ebdd4322cb760183de299f915c4778f
enrique1994 <enrique_face1994@hotmail.com>
2018-02-14 11:58:51 -0600

    Anadir_Anuncios
    Añadir anuncios de parte del profesor

--- php/editar_datos_curso.php
+++ php/editar_datos_curso.php
@@ -31,6 +31,13 @@ if (mysqli_query($conn, $sql)) {
 }
 
 
+$sql = "UPDATE anuncio set id_curso=$nrc2 WHERE id_curso=$nrc";
+$result = $conn->query($sql);
+if (mysqli_query($conn, $sql)) {
+    $con=$con+1;
+}
+
+
 $sql = "UPDATE inscripcion set id_curso=$nrc2 WHERE id_curso=$nrc";
 $result = $conn->query($sql);
 if (mysqli_query($conn, $sql)) {
@@ -52,7 +59,7 @@ if (mysqli_query($conn, $sql)) {
 } 
 
 
-if ($con ==5) {
+if ($con ==6) {
 echo '<script language="javascript">alert("Valores editados correctamente")</script>'; 
 echo '<script language="javascript">window.location.href="../vistas/cursos.php" ;</script>'; 
 }else{
--- vistas/home_alumno.php
+++ vistas/home_alumno.php
@@ -1,6 +1,5 @@
 <?php
-//include_once '../php/conexion.php';
-/*
+include_once '../php/conexion.php';
 if(!$alumno->is_loggedin())
 {
  $alumno->redirect('../index.html');
@@ -9,14 +8,13 @@ $user_id = $_SESSION['user_session'];
 $stmt = $DB_con->prepare("SELECT * FROM alumno WHERE matricula=:user_id");
 $stmt->execute(array(":user_id"=>$user_id));
 $userRow=$stmt->fetch(PDO::FETCH_ASSOC);
-*/
 ?>
 <!DOCTYPE html>
 <html>
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>SSAP | Página principal del alumno</title>
+  <title>SSAP | Administrar cursos</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.6 -->
@@ -37,15 +35,6 @@ $userRow=$stmt->fetch(PDO::FETCH_ASSOC);
   <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
   <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
   <![endif]-->
-     <style>
-		.anuncio {
-    		background-color: #e7f3fe;
-    		border-left: 6px solid #2196F3;
-		}
-    </style>
-    <script>
-     var idalumno = '<?php echo $user_id; ?>'; 
-    </script>
 </head>
 <body class="hold-transition skin-blue sidebar-mini">
 <!-- Site wrapper -->
@@ -198,7 +187,7 @@ $userRow=$stmt->fetch(PDO::FETCH_ASSOC);
               <!-- Menu Footer-->
               <li class="user-footer">
                 <div class="pull-left">
-                  <a href="ver_perfil_alumno.php" class="btn btn-default btn-flat">Perfil</a>
+                  <a href="ver_perfil.php" class="btn btn-default btn-flat">Perfil</a>
                 </div>
                 <div class="pull-right">
                   <a href="../php/logout.php" class="btn btn-default btn-flat">Cerrar Sesión</a>
@@ -276,7 +265,7 @@ $userRow=$stmt->fetch(PDO::FETCH_ASSOC);
           </ul>
         </li>
         -->
-         <li>
+        <li>
           <a href="home_alumno.php">
             <i class="glyphicon glyphicon-home"></i> <span>Inicio</span>
             <span class="pull-right-container">
@@ -284,7 +273,7 @@ $userRow=$stmt->fetch(PDO::FETCH_ASSOC);
             </span>
           </a>
         </li>
-        <li>
+                <li>
           <a href="ver_cursos.php">
             <i class=" fa fa-book"></i> <span>Ver Cursos</span>
             <span class="pull-right-container">
@@ -292,7 +281,7 @@ $userRow=$stmt->fetch(PDO::FETCH_ASSOC);
             </span>
           </a>
         </li>
-        <li>
+         <li>
           <a href="<?php print'historial.php?matricula='.($userRow['matricula'])?>">
             <i class=" fa fa-list-alt"></i> <span>Ver Historial</span>
             <span class="pull-right-container">
@@ -308,17 +297,6 @@ $userRow=$stmt->fetch(PDO::FETCH_ASSOC);
             </span>
           </a>
         </li>
-
-        <li>
-          <a href="aplicaciones.php">
-            <i class="glyphicon glyphicon-th"></i> <span>Aplicaciones</span>
-            <span class="pull-right-container">
-              <!--<small class="label pull-right bg-green">Hot</small> -->
-            </span>
-          </a>
-        </li>
-          
-        
         <!--
         <li class="treeview">
           <a href="#">
@@ -466,7 +444,7 @@ $userRow=$stmt->fetch(PDO::FETCH_ASSOC);
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <h1>
-        Página principal de Alumno
+       Cursos 
        <!-- <small>it all starts here</small>-->
       </h1>
 
@@ -481,32 +459,91 @@ $userRow=$stmt->fetch(PDO::FETCH_ASSOC);
 
     <!-- Main content -->
     <section class="content">
-
-      <!-- Default box -->
-      <div class="box">
-        <div class="box-header with-border">
-          <h3 class="box-title">Anuncios</h3>
-
-          <div class="box-tools pull-right">
-            <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse">
-              <i class="fa fa-minus"></i></button>
-            <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
-              <i class="fa fa-times"></i></button>
-          </div>
-        </div>
-        <div class="box-body">
-          <div id="anuncios" class="infox">
-		          	
-	          	</div>
-        </div>
-        <!-- /.box-body -->
-        <div class="box-footer">
-          
-        </div>
-        <!-- /.box-footer-->
-      </div>
+     
       <!-- /.box -->
+<div class="row">
+        <div class="col-xs-12">
+          <div class="box">
+            <div class="box-header">
+             
+              <br>
+                
+              <div class="box-tools">
+                <div class="input-group input-group-sm" style="width: 150px;">
+                  <input type="text" name="table_search" class="form-control pull-right" placeholder="Search">
 
+                  <div class="input-group-btn">
+                    <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body table-responsive no-padding">
+            <div ng-app="historial" ng-controller="principal">
+  
+              <table class="table table-hover">
+                <tr>
+                 <th>NRC</th>
+        <th>Profesor</th>
+        <th>Anuncio>
+                </tr>
+    <?php
+$stmt1 = $DB_con->prepare("SELECT * FROM inscripcion WHERE id_alumno =:user_id");
+$stmt1->execute(array(":user_id"=>$user_id));
+while($ins=$stmt1->fetch(PDO::FETCH_ASSOC)){
+?>
+
+<tr>
+    <td>
+<?php
+echo $ins['id_curso'];
+    ?>
+    </td>
+
+<td>
+<?php
+$stmt5 = $DB_con->prepare("SELECT * FROM profesor WHERE id =:id_pro");
+$stmt5->execute(array(":id_pro"=>$cur['id_profesor']));
+$prof=$stmt5->fetch(PDO::FETCH_ASSOC);
+
+echo $prof['nombre']." ".$prof['paterno']." ".$prof['materno'];
+  ?>
+</td>
+
+ 
+  <td>
+    <?php
+$stmt2 = $DB_con->prepare("SELECT * FROM anuncio WHERE nrc =:id_mat");
+$stmt2->execute(array(":id_mat"=>$ins['id_curso']));
+$cur=$stmt2->fetch(PDO::FETCH_ASSOC);
+while($cur=$stmt1->fetch(PDO::FETCH_ASSOC)){
+echo $cur['descripcion'];
+}
+    ?>
+    </td>
+
+
+
+
+
+      </tr>
+<?php
+}
+?>
+
+      <!--Aqui acaba el renglon
+
+
+              </table>
+              </div>
+            </div>
+            <!-- /.box-body -->
+           
+          </div>
+          <!-- /.box -->
+          
+         
     </section>
     <!-- /.content -->
   </div>
@@ -718,7 +755,7 @@ $userRow=$stmt->fetch(PDO::FETCH_ASSOC);
 <!-- ./wrapper -->
 
 <!-- jQuery 2.2.3 -->
-<script src="../js/jquery-3.1.1.min.js"></script>
+<script src="../plugins/jQuery/jquery-2.2.3.min.js"></script>
 <!-- Bootstrap 3.3.6 -->
 <script src="../bootstrap/js/bootstrap.min.js"></script>
 <!-- SlimScroll -->
@@ -729,6 +766,7 @@ $userRow=$stmt->fetch(PDO::FETCH_ASSOC);
 <script src="../dist/js/app.min.js"></script>
 <!-- AdminLTE for demo purposes -->
 <script src="../dist/js/demo.js"></script>
-<script src="../scripts/getAnunciosAlum.js"></script>
+<script src="../js/angular.min.js"></script>
+<script src="../scripts/historial.js"></script>
 </body>
 </html>
