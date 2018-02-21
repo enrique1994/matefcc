7d1f1d7afdf9a2b16e81f1830ddc41fb401db434
enrique1994 <enrique_face1994@hotmail.com>
2018-02-21 08:42:32 -0600

    _
    _

--- vistas/criterios.php
+++ vistas/criterios.php
@@ -501,10 +501,10 @@ if ($cri['id_parcial_cri']==$conteo_parcial) {
   echo "<tr>";
   echo "<td></td>";
   echo "<td>Examen</td>";
-$stmt1 = $DB_con->prepare("SELECT * FROM evaluacion WHERE nrc_curso=:nrc_cri");
-$stmt1->execute(array(":nrc_cri"=>$cri['id']));
-    $subcri=$stmt1->fetch(PDO::FETCH_ASSOC);
-  echo "<td><a href=num_eje.php?id=$subcri[id]>Alta</a></td>";
+$stmt2 = $DB_con->prepare("SELECT * FROM evaluacion WHERE nrc_curso=:nrc_cri");
+$stmt2->execute(array(":nrc_cri"=>$cri['id']));
+  $subcri=$stmt2->fetch(PDO::FETCH_ASSOC);
+  echo "<td><a href=num_eje.php?id=$subcri['id']>Alta</a></td>";
 
   echo "<td>Eliminar</td>";
   echo "</tr>";
@@ -519,7 +519,7 @@ $stmt1->execute(array(":nrc_cri"=>$cri['id']));
     $subcri=$stmt1->fetch(PDO::FETCH_ASSOC);
     # code...
   }
-  echo "<td><a href=num_eje.php?id=$subcri[id]>Alta</a></td>";
+  echo "<td><a href=num_eje.php?id=$subcri['id']>Alta</a></td>";
   echo "<td>Eliminar</td>";
   echo "</tr>";
   }
