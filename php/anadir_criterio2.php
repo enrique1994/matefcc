131eca23e7dee5d9bdfdced9575f02e5ebf3e54e
enrique1994 <enrique_face1994@hotmail.com>
2018-03-01 12:25:05 -0600

    _
    _

--- php/anadir_criterio2.php
+++ php/anadir_criterio2.php
@@ -10,7 +10,7 @@ $stmt->execute(array(":user_id"=>$user_id));
 $userRow=$stmt->fetch(PDO::FETCH_ASSOC);
 
       $stmt3 = $DB_con->prepare("UPDATE  evaluacion SET maximo_ejer=:ej, titulo=:titulo where id=:id");
-      $stmt3->bindParam(":id",$$_POST['id']);
+      $stmt3->bindParam(":id",$_POST['id']);
       $stmt3->bindParam(":titulo",$_POST['titulo']);
       $stmt3->bindParam(":ej",$_POST['porcentaje']);
         if($stmt3->execute())
