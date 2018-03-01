fe6a40285fb72ea2aaa44e97947ef895d33265cf
enrique1994 <enrique_face1994@hotmail.com>
2018-03-01 14:54:30 -0600

    _
    _

--- php/anadir_criterio2.php
+++ php/anadir_criterio2.php
@@ -13,9 +13,9 @@ echo $_POST['id'];
 echo $_POST['titulo'];
 echo $_POST['porcentaje'];
       $stmt3 = $DB_con->prepare("UPDATE  evaluacion SET maximo_ejer=:ej, titulo=:titulo where id=:id");
-      $stmt3->bindParam(":id",$_POST['id']);
-      $stmt3->bindParam(":titulo",$_POST['titulo']);
-      $stmt3->bindParam(":ej",$_POST['porcentaje']);
+      $stmt3->bindParam(":id",$_GET['id']);
+      $stmt3->bindParam(":titulo",$_GET['titulo']);
+      $stmt3->bindParam(":ej",$_GET['porcentaje']);
         if($stmt3->execute())
         {
  echo '<script language="javascript">alert("Sub criterio de evaluacion actualizado")</script>'; 
