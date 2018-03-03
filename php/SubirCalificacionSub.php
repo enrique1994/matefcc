d14ca06c9a90bfd1eac166d85e9789015cde694c
enrique1994 <enrique_face1994@hotmail.com>
2018-03-03 14:16:35 -0600

    _
    _

--- php/SubirCalificacionSub.php
+++ php/SubirCalificacionSub.php
@@ -8,7 +8,7 @@ $user_id = $_SESSION['user_session'];
 $cuenta=0;
 //echo $hola[24];
 
-
+$calif=$_POST['calif'];
 $stmt1 = $DB_con->prepare("UPDATE evaluacion set calif=$calif/maximo_ejer*10, num_ejer=:calif, hora=0 WHERE id=:id");
 $stmt1->bindParam(":calif",$_POST['calif']);
 $stmt1->bindParam(":id",$_POST['id']);
@@ -26,15 +26,5 @@ echo '<script language="javascript">window.location.href="../vistas/ver_cursos.p
         }
 
 
-if($cuenta>0)
-{
-echo '<script language="javascript">alert("Error al subir la calificacion ")</script>'; 
-echo '<script language="javascript">window.location.href="../vistas/cursos.php" ;</script>'; 
-}
-else
-{
-echo '<script language="javascript">alert("Calificaciones subidas correctamente")</script>'; 
-echo '<script language="javascript">window.location.href="../vistas/ver_cursos.php" ;</script>'; 
-}
 
 ?>
