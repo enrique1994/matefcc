db1984193a8884f133e3b0d3a0d280c69dbbbd5c
enrique1994 <enrique_face1994@hotmail.com>
2018-03-03 13:07:42 -0600

    _
    _

--- php/SubirCalificacionSub.php
+++ php/SubirCalificacionSub.php
@@ -5,21 +5,24 @@ if(!$alumno->is_loggedin())
  $alumno->redirect('../index.html');
 }
 $user_id = $_SESSION['user_session'];
-
-
-
 $cuenta=0;
 //echo $hola[24];
-echo $_POST['calif'];
-echo $_POST['id'];
 
 $stmt1 = $DB_con->prepare("UPDATE evaluacion set calif=$calif/maximo_ejer*10, num_ejer=:calif, hora=0 WHERE id=:id");
 $stmt1->bindParam(":calif",$_POST['calif']);
 $stmt1->bindParam(":id",$_POST['id']);
-$stmt1->execute();
 
 
 
+        if($stmt1->execute())
+        {
+echo '<script language="javascript">alert("Calificacion subida correctamente")</script>'; 
+echo '<script language="javascript">window.location.href="../vistas/ver_cursos.php" ;</script>'; 
+        }
+        else
+        {
+          echo "No se puede ejecutar !";
+        }
 
 
 if($cuenta>0)
