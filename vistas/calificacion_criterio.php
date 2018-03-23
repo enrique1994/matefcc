78156c09e2d6d185c4d20080174fdf74b02d1950
enrique1994 <enrique_face1994@hotmail.com>
2018-03-22 22:58:38 -0600

    Anadir calificacion por criterio
    Anadir las calificaciones por criterio

--- vistas/calificacion_criterio.php
+++ vistas/calificacion_criterio.php
@@ -8,38 +8,25 @@ $user_id = $_SESSION['user_session'];
 $stmt = $DB_con->prepare("SELECT * FROM profesor WHERE id=:user_id");
 $stmt->execute(array(":user_id"=>$user_id));
 $userRow=$stmt->fetch(PDO::FETCH_ASSOC);
+$nrc=$_GET['a'];
+$promedio=0;
+
+
+$stmt2 = $DB_con->prepare("SELECT * FROM inscripcion WHERE id_curso=:user_id");
+$stmt2->execute(array(":user_id"=>$nrc));
+
+$stmt4 = $DB_con->prepare("SELECT * FROM parcial WHERE id_curso_parcial=:user_id");
+$stmt4->execute(array(":user_id"=>$nrc));
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
-<?php
 
-$servername = "us-cdbr-azure-southcentral-f.cloudapp.net";
-$username = "b9a859ae739783";
-$password = "903a2e5b";
-$dbname = "matefcc";
-/*
-$DB_host = "us-cdbr-azure-southcentral-f.cloudapp.net";
-$DB_user = "b9a859ae739783";
-$DB_pass = "903a2e5b";
-$DB_name = "matefcc";
-*/
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
 
-$sql = "SELECT * FROM curso WHERE id_profesor= $user_id";
-$result = $conn->query($sql);
-
-?>
 <!DOCTYPE html>
 <html>
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>SSAP | Administrar cursos</title>
+  <title>SSAP | Calificaciones finales</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.6 -->
@@ -134,7 +121,7 @@ $result = $conn->query($sql);
                     <a href="#">
                       <i class="fa fa-users text-aqua"></i> 5 new members joined today
                     </a>
-                  </li>
+                  </li>   
                 </ul>
               </li>
               <li class="footer"><a href="#">View all</a></li>
@@ -482,11 +469,8 @@ $result = $conn->query($sql);
         <div class="col-xs-12">
           <div class="box">
             <div class="box-header">
-              <h3 class="box-title">Cursos Inscritos</h3>
+              <h3 class="box-title">Calificación final</h3>
               <br>
-                <a href="agregar_curso.php">
-                 <i class="glyphicon glyphicon-plus"></i> <span>Añadir Curso</span>
-                    </a>
               <div class="box-tools">
                 <div class="input-group input-group-sm" style="width: 150px;">
                   <input type="text" name="table_search" class="form-control pull-right" placeholder="Search">
@@ -501,85 +485,46 @@ $result = $conn->query($sql);
             <div class="box-body table-responsive no-padding">
               <table class="table table-hover">
                 <tr>
-                  <th>NRC</th>
-                  <th>Código</th>
-                  <th>Nombre</th>
-                  <th>Sección</th>
-                  <th>Periodo</th>
-
-                  <th># de Alumnos</th>
-                  <th>Acciones sobre alumnos</th>
-                  <th>Criterios de evaluación</th>
-                  <th>Calificacion criterio</th>
-                  <th>Calificacion final</th>
-                  <th>Anuncios</th>
-                  <th>Editar</th>
-                  <th>Eliminar</th>
+                  <th>Parcial</th>
+                  <th>Matricula</th>
+                  <th>Calificacion</th>
                 </tr>
-               <tr>
-<?php
-    while($row = $result->fetch_array()) {
-?>
-	<td> <a href="salon.php?a=<?php echo $row[0] ?>"> <?php echo $row[0] ?></a> 
-	<td><?php  
-	$sql1 = "SELECT * FROM materia WHERE id= $row[1]";
-$result1 = $conn->query($sql1);
-$row1 = $result1->fetch_array();
-	echo $row1[1]; 
-	$id=$row1[1];?> 
-</td>
-	<td> <?php  
-	$sql1 = "SELECT * FROM materia WHERE id= $row[1]";
-$result1 = $conn->query($sql1);
-$row1 = $result1->fetch_array();
-	echo $row1[2]; ?> 
-	</td>
-
-	<td>
-		 <?php  
-	$sql2 = "SELECT * FROM seccion WHERE id= $row[2]";
-$result2 = $conn->query($sql2);
-$row2 = $result2->fetch_array();
-	echo $row2[1]; ?>  
-	</td>
-	<td>
-				 <?php  
-	$sql3 = "SELECT * FROM periodo WHERE id= $row[4]";
-$result3 = $conn->query($sql3);
-$row3 = $result3->fetch_array();
-	echo $row3[1].$row3[2]; ?>
-	</td>
-
-    <td><?php  
-	$sql1 = "SELECT COUNT(*) FROM inscripcion where id_curso=$row[0]";
-$result1 = $conn->query($sql1);
-$row1 = $result1->fetch_array();
-	echo $row1[0]; 
-?> </td>
+  
+                  <?php while($criterio=$stmt4->fetch(PDO::FETCH_ASSOC)){?>
+	<tr>
 <td>
-	<a href="insertar_alumnos.php?a=<?php echo $row[0] ?>">Insertar alumnos</a>
-	<br>
-	<a href="editar_alumnos.php?a=<?php echo $row[0] ?>">Editar alumnos</a>
-	<br>
-	<a href="eliminar_alumnos.php?a=<?php echo $row[0] ?>">Eliminar alumnos</a>
+  <?php
+echo $criterio['num_p'];
+  ?>
 </td>
-<td><a href="criterios.php?a=<?php echo $row[0] ?>">Criterios</a></td>
-<td><a href="calificacion_criterio.php?a=<?php echo $row[0] ?>">Calificacion criterio</a></td>
-<td><a href="calificacion_final.php?a=<?php echo $row[0] ?>">Calificacion final</a></td>
-<td><a href="mis_anuncios.php?id_curso=<?php echo $row[0] ?>">Ver anuncios</a></td>
-  <td><a href="editar_curso.php?a=<?php echo $row[0] ?>" class="glyphicon glyphicon-edit"></a></td>
-    <td><a href="../php/eliminar_curso.php?a=<?php echo $row[0] ?>" class="glyphicon glyphicon-remove"></a></td>
 
+<td>
+  <?php
+echo $criterio['matricula'];
+  ?>
+</td>
+
+<td>
+  <?php
+echo $criterio['calif'];
+  ?>
+</td>
 
 </tr>
-
 <?php
 }
 ?>
+
+
               </table>
+              <?php
+ echo '<script language="javascript">alert("Calificaciones actualizadas")</script>'; 
+//echo '<script language="javascript">window.location.href="home.php" ;</script>'; 
+ 
+?>
             </div>
             <!-- /.box-body -->
-           
+    
           </div>
           <!-- /.box -->
           
